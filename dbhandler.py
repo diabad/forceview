d427f52b4c3a90b1f1d8bb064fcfb76db1365afe
Aaron Diab <diabad@gmail.com>
2021-03-25 23:14:15 -0700

    dbhandler class can now create the db, insert the patient table and insert a patient.

--- dbhandler.py
+++ dbhandler.py
@@ -17,13 +17,60 @@ class dbHandler:
     def __init__(self):
         #Get the applications base directory
         self.progDir = os.path.dirname(os.path.abspath(__file__))
+        #Set the dbFile name so the database can be opened
+        self.dbFile = self.progDir + '\\fvdatabase.db'
+        #Establish db connection
+        self.dbConn = self.connectToDB(self.dbFile)
 
-    #def dbConn(dbfile):
+    #Establishes a connection to the database
+    def connectToDB(self, dbfile):
+        #Initialize the connection variable
+        dbConn = None
+        try:
+            dbConn = sqlite3.connect(dbfile)
+            return dbConn
+        except Error as err:
+            print(err)
+        
+        return dbConn
 
-    #def run_startup(self):
-        #self.root.mainloop()
+    
+    def createTable(self, dbconn, tableSQL):
+        #Create the database cursor
+        try:
+            dbCur = dbconn.cursor()
+            dbCur.execute(tableSQL)
+        except Error as err:
+            print(err)
+
+    def createPatTable(self):
+        #SQL to create the patient table
+        patTableSQL = """ CREATE TABLE IF NOT EXISTS patients (
+                            id integer PRIMARY KEY,
+                            last_name text NOT NULL,
+                            first_name text NOT NULL,
+                            date_of_birth text
+                        ); """
+        if self.dbConn is not None:
+            self.createTable(self.dbConn, patTableSQL)
+        else:
+            print("Error opening connection to database!")
+
+    def createPatient(self, patient):
+        #SQL to insert a new patient into the database
+        sql = ''' INSERT INTO patients(last_name,first_name,date_of_birth) VALUES(?,?,?) '''
+
+        #Create the db cursor
+        dbCur = self.dbConn.cursor()
+        dbCur.execute(sql, patient)
+        self.dbConn.commit()
+
+        return dbCur.lastrowid
 
 if __name__ == '__main__':
     #Make an instance of the application and run it
     dh = dbHandler()
-    print(dh.progDir)
+    dh.createPatTable()
+    patient = ('Doe', 'John', '1-1-1945')
+    dh.createPatient(patient)
+
